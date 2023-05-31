c0fe967b13dcb1936d7dd82b113185f937fd92a4
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-05-31 14:10:28 +0200

    Don't retry requests or show error toast for 404 (i.e. for person search)
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

---
+++ frontend/src/simple-api-state/types.ts
@@ -0,0 +1,28 @@
+export interface State<T> {
+  data: T | undefined;
+  isLoading: boolean;
+  isUninitialized: boolean;
+  isError: boolean;
+  error: Error | undefined;
+  isSuccess: boolean;
+  updateData: UpdateData<T>;
+}
+
+export interface InternalOptions {
+  prefetch: boolean;
+  cacheTime: number;
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+}
+
+export type RequestBody = Record<string, unknown> | string | undefined;
+
+export type Options = Partial<InternalOptions> | undefined;
+
+export type Listener<T> = (state: State<T>) => void;
+export type UpdateData<T> = (updater: (data: T | undefined) => T | undefined) => void;
+
+export class NoRetryError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+}
