22155531c4e9a2ec2b39d092bf0ca75964da6bfe
Daniel Eriksson <eriksson.daniel@protonmail.com>
2023-08-07 13:27:48 +0200

    Update dependencies
    Co-authored-by: Christian Skrøvseth <cskrov@users.noreply.github.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -53,7 +53,7 @@ server.use(
       'X-Requested-With',
     ],
     origin: isDeployedToProd ? DOMAIN : [DOMAIN, /https?:\/\/localhost:\d{4,}/],
-  })
+  }),
 );
 
 server.get('/isAlive', (req, res) => res.status(200).send('Alive'));
