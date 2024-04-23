13696cdeeb2ce69de0fa1d50576ac797f1c92ad8
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-04-23 13:29:05 +0200

    Fix default selected recipients logic

--- frontend/src/functions/is-not.ts
+++ frontend/src/functions/is-not.ts
@@ -1,1 +1,3 @@
 export const isNotNull = <T>(value: T | null): value is T => value !== null;
+export const isNotNullNorUndefined = <T>(value: T | null | undefined): value is T =>
+  value !== null && value !== undefined;
