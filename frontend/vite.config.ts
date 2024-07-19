cedaa17bfa1e2cfec164199578e5c591af34021b
Daniel Eriksson <eriksson.daniel@protonmail.com>
2024-07-19 12:56:18 +0200

    Use Bun/Vite and consolidate with Kabal

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  server: {
+    port: 8063,
+    proxy: {
+      '/api': 'https://kabin.intern.dev.nav.no',
+      '/version': 'https://kabin.intern.dev.nav.no',
+    },
+  },
+})
+
