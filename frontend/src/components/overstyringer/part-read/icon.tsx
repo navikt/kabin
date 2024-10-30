67e16fda2f085fe62232ef605cd484821e788bd5
Christian Skrøvseth <christian.skrovseth@gmail.com>
2024-10-30 16:47:01 +0100

    Update dependencies

--- frontend/src/components/overstyringer/part-read/icon.tsx
+++ frontend/src/components/overstyringer/part-read/icon.tsx
@@ -1,5 +1,5 @@
 import { type AvsenderMottakerType, IdType } from '@app/types/common';
-import { Buldings2Icon, PersonIcon, QuestionmarkIcon } from '@navikt/aksel-icons';
+import { Buildings2Icon, PersonIcon, QuestionmarkIcon } from '@navikt/aksel-icons';
 
 interface Props {
   type?: IdType | AvsenderMottakerType | null;
@@ -7,7 +7,7 @@ interface Props {
 
 export const Icon = ({ type = null }: Props) => {
   if (type === IdType.ORGNR) {
-    return <Buldings2Icon aria-hidden fontSize={20} />;
+    return <Buildings2Icon aria-hidden fontSize={20} />;
   }
 
   if (type === IdType.FNR) {
