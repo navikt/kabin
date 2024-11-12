1315dea8d4b2e5468c79730fede0669e8c84f533
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-11-12 23:03:41 +0100

    Simplify use check for Gosys Oppgave.
    Co-authored-by: Andreas Jonsson <andreas.jonsson@nav.no>

--- frontend/src/components/gosys-oppgaver/hooks.tsx
+++ frontend/src/components/gosys-oppgaver/hooks.tsx
@@ -2,8 +2,6 @@ import { useMulighet } from '@app/hooks/use-mulighet';
 import { useRegistrering } from '@app/hooks/use-registrering';
 import { useGetFagsystemerQuery } from '@app/redux/api/kodeverk';
 import type { IGetGosysOppgaverParams } from '@app/redux/api/oppgaver';
-import { SaksTypeEnum } from '@app/types/common';
-import { FagsystemId } from '@app/types/mulighet';
 import { skipToken } from '@reduxjs/toolkit/query/react';
 
 export const useParams = (): IGetGosysOppgaverParams | typeof skipToken => {
@@ -31,17 +29,9 @@ export const useIsEnabled = () => {
 
   const fagsystem = fagsystemer.find(({ id }) => id === mulighet.originalFagsystemId);
 
-  if (fagsystem === undefined || fagsystem.modernized) {
+  if (fagsystem === undefined) {
     return false;
   }
 
-  if (typeId === SaksTypeEnum.KLAGE || typeId === SaksTypeEnum.OMGJØRINGSKRAV) {
-    return true;
-  }
-
-  if (typeId === SaksTypeEnum.ANKE && mulighet.originalFagsystemId === FagsystemId.INFOTRYGD) {
-    return true;
-  }
-
-  return false;
+  return !fagsystem.modernized;
 };
