36fdb9dbce2c223c04082c9134279c7dd85d19e5
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-04-09 15:49:15 +0200

    WIP

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,9 +1,13 @@
 import path from 'node:path';
 import { isLocal } from '@app/config/env';
-import { requiredEnvJson, requiredEnvString } from '@app/config/env-var';
-import type { JWK } from 'jose';
+import { requiredEnvString } from '@app/config/env-var';
 
-export const API_CLIENT_IDS = ['kabin-api', 'kabal-api', 'kabal-innstillinger', 'klage-kodeverk-api'];
+const KABIN_API = 'kabin-api';
+const KABAL_API = 'kabal-api';
+const KABAL_INNSTILLINGER = 'kabal-innstillinger';
+export const KLAGE_KODEVERK_API = 'klage-kodeverk-api';
+
+export const API_CLIENT_IDS = [KABIN_API, KABAL_API, KABAL_INNSTILLINGER, KLAGE_KODEVERK_API];
 
 const cwd = process.cwd(); // This will be the server folder, as long as the paths in the NPM scripts are not changed.
 const serverDirectoryPath = cwd;
@@ -11,11 +15,7 @@ export const frontendDirectoryPath = path.resolve(serverDirectoryPath, '../front
 export const frontendDistDirectoryPath = path.resolve(frontendDirectoryPath, './dist');
 
 const defaultValue = isLocal ? 'local' : undefined;
-const localJwk: JWK = { kty: 'RSA' };
 
-export const AZURE_APP_CLIENT_ID = requiredEnvString('AZURE_APP_CLIENT_ID', defaultValue);
-export const AZURE_APP_WELL_KNOWN_URL = requiredEnvString('AZURE_APP_WELL_KNOWN_URL', defaultValue);
-export const AZURE_APP_JWK = requiredEnvJson<JWK>('AZURE_APP_JWK', localJwk);
 export const PROXY_VERSION = requiredEnvString('VERSION', defaultValue);
 export const PORT = requiredEnvString('PORT', '8080');
 export const NAIS_CLUSTER_NAME = requiredEnvString('NAIS_CLUSTER_NAME', defaultValue);
