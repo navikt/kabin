56ddc2828c86e91abc9b3063fe31e5d0731fc232
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-08-08 15:04:59 +0200

    Make column headers readable in dark mode

--- frontend/src/pages/index/finished/headers.tsx
+++ frontend/src/pages/index/finished/headers.tsx
@@ -1,27 +1,35 @@
-import { Table } from '@navikt/ds-react';
+import { SortHeader } from '@app/pages/index/finished/sort-header';
+import { type SortState, Table, type TableProps } from '@navikt/ds-react';
 
-export const FinishedHeaders = () => (
-  <Table.Header>
-    <Table.Row>
-      <Table.ColumnHeader colSpan={2}>Saken gjelder</Table.ColumnHeader>
+interface Props {
+  sortState: SortState;
+  onSortChange: TableProps['onSortChange'];
+}
 
-      <Table.ColumnHeader sortKey="typeId" sortable>
-        Type
-      </Table.ColumnHeader>
+export const FinishedHeaders = ({ sortState, onSortChange }: Props) => {
+  const { direction, orderBy } = sortState;
+  const common = { direction, orderBy, onSortChange };
 
-      <Table.ColumnHeader sortKey="ytelseId" sortable>
-        Ytelse
-      </Table.ColumnHeader>
+  return (
+    <Table.Header>
+      <Table.Row>
+        <Table.ColumnHeader colSpan={2}>Saken gjelder</Table.ColumnHeader>
 
-      <Table.ColumnHeader sortKey="created" sortable>
-        Opprettet
-      </Table.ColumnHeader>
+        <SortHeader {...common} sortKey="typeId">
+          Type
+        </SortHeader>
+        <SortHeader {...common} sortKey="ytelseId">
+          Ytelse
+        </SortHeader>
+        <SortHeader {...common} sortKey="created">
+          Opprettet
+        </SortHeader>
+        <SortHeader {...common} sortKey="finished">
+          Fullført
+        </SortHeader>
 
-      <Table.ColumnHeader sortKey="finished" sortable>
-        Fullført
-      </Table.ColumnHeader>
-
-      <Table.HeaderCell />
-    </Table.Row>
-  </Table.Header>
-);
+        <Table.HeaderCell />
+      </Table.Row>
+    </Table.Header>
+  );
+};
