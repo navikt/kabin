e6d9ec6490de6073423c8a9eb7b608a449406e85
Daniel Eriksson <eriksson.daniel@protonmail.com>
2025-10-17 10:14:30 +0200

    Link to Kabal on same domain as user's

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,13 @@
 import { ENVIRONMENT } from '@app/environment';
 
-export const KABAL_URL = ENVIRONMENT.isProduction ? 'https://kabal.intern.nav.no' : 'https://kabal.intern.dev.nav.no';
+export const getKabalUrl = (): string => {
+  const hostname = window.location.hostname;
+
+  if (hostname.includes('.ansatt.nav.no')) {
+    return 'https://kabal.ansatt.nav.no';
+  }
+
+  return ENVIRONMENT.isProduction ? 'https://kabal.intern.nav.no' : 'https://kabal.intern.dev.nav.no';
+};
+
+export const KABAL_URL = getKabalUrl();
