dcd08e90aaf936f59e175aff011cab4f4747899f
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2025-12-11 10:18:50 +0100

    Show why avsender cannot be changed

--- frontend/src/components/overstyringer/avsender.tsx
+++ frontend/src/components/overstyringer/avsender.tsx
@@ -1,12 +1,14 @@
 import { StyledAvsenderIcon } from '@app/components/overstyringer/icons';
 import { Part } from '@app/components/overstyringer/part';
 import type { ISetPart } from '@app/components/overstyringer/part-read/types';
+import { PartContent, States, StyledContainer } from '@app/components/overstyringer/styled-components';
 import { FieldNames } from '@app/components/overstyringer/types';
 import { useJournalpost } from '@app/hooks/use-journalpost';
 import { useRegistrering } from '@app/hooks/use-registrering';
 import { useValidationError } from '@app/hooks/use-validation-error';
 import { JournalposttypeEnum } from '@app/types/dokument';
 import { ValidationFieldNames } from '@app/types/validation';
+import { Heading, InfoCard } from '@navikt/ds-react';
 
 interface Props {
   options: ISetPart[] | undefined;
@@ -18,12 +20,29 @@ export const Avsender = ({ options }: Props) => {
   const { journalpost } = useJournalpost();
   const error = useValidationError(ValidationFieldNames.AVSENDER);
 
-  if (
-    journalpost === undefined ||
-    journalpost.journalposttype !== JournalposttypeEnum.INNGAAENDE ||
-    !journalpost.canChangeAvsender
-  ) {
+  if (journalpost === undefined || journalpost.journalposttype !== JournalposttypeEnum.INNGAAENDE) {
     return null;
+  }
+
+  if (!journalpost.canChangeAvsender) {
+    return (
+      <StyledContainer $state={States.UNSET}>
+        <StyledAvsenderIcon aria-hidden />
+        <PartContent>
+          <Heading level="3" size="xsmall">
+            Avsender
+          </Heading>
+          <InfoCard data-color="info" size="small">
+            <InfoCard.Header>
+              <InfoCard.Title>Avsender kan ikke endres</InfoCard.Title>
+            </InfoCard.Header>
+            <InfoCard.Content>
+              Avsender kan ikke endres på journalposter som er eldre enn ett år eller som er sendt inn digitalt.
+            </InfoCard.Content>
+          </InfoCard>
+        </PartContent>
+      </StyledContainer>
+    );
   }
 
   return (
