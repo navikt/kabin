af851507dace882acab6a092d37ed03543aba302
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-03-05 11:08:40 +0100

    Finished row: Remove status prefetch

--- frontend/src/pages/index/finished/row.tsx
+++ frontend/src/pages/index/finished/row.tsx
@@ -8,17 +8,15 @@ import { DateTime } from '@app/pages/index/table-components/datetime';
 import { OpenButton } from '@app/pages/index/table-components/open';
 import { SakenGjelder } from '@app/pages/index/table-components/saken-gjelder';
 import type { FinishedRegistreringListItem } from '@app/redux/api/registreringer/types';
-import { usePrefetch } from '@app/redux/api/status';
 import { HStack, Table } from '@navikt/ds-react';
 
 export const FinishedRow = ({ registrering }: { registrering: FinishedRegistreringListItem }) => {
-  const { id, sakenGjelderValue, typeId, ytelseId, created, finished, behandlingId } = registrering;
-  const prefetchStatus = usePrefetch('getStatus');
+  const { id, sakenGjelderValue, typeId, ytelseId, created, finished } = registrering;
 
   const path = `/registrering/${id}/status`;
 
   return (
-    <TableRow path={path} onMouseEnter={() => prefetchStatus(behandlingId)}>
+    <TableRow path={path}>
       <SakenGjelder id={sakenGjelderValue} />
 
       <Table.DataCell>
