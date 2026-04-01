1dc069f261c5821612f25adcb804e09fa7432678
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-04-01 17:44:36 +0200

    Use TS config files for Knip

--- server/knip.ts
+++ server/knip.ts
@@ -1,9 +1,9 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['index.css'],
   project: ['src/**'],
   bun: {
+    config: ['package.json'],
     entry: ['**/*.test.{ts,tsx}'],
   },
 };
