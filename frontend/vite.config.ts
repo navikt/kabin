bbd2527ad64dd75b8df6a86ff8842757393e3a83
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-04-01 15:17:18 +0200

    Remove unnecessary Vite plugin for TS paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ command }) => ({
-  plugins: [tsconfigPaths(), react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   base: command === 'build' ? 'https://cdn.nav.no/klage/kabin/' : '/',
   build: {
     sourcemap: true,
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   server: {
     port: 8063,
