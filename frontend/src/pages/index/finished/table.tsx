13a4e80c2024f0e6bceb266d37f312097fb4c9e5
Daniel Eriksson <11520860+eriksson-daniel@users.noreply.github.com>
2026-09-10 13:12:06 +0200

    Avoid too wide finished registreringer table

--- frontend/src/pages/index/finished/table.tsx
+++ frontend/src/pages/index/finished/table.tsx
@@ -24,7 +24,7 @@ export const FinishedTable = ({ registreringer, sortState, onSortChange }: Regis
 
   return (
     <TableAndPagination>
-      <Table zebraStripes size="small" sort={sortState} onSortChange={onSortChange} style={{ whiteSpace: 'nowrap' }}>
+      <Table zebraStripes size="small" sort={sortState} onSortChange={onSortChange} className="w-fit whitespace-nowrap">
         <FinishedHeaders sortState={sortState} onSortChange={onSortChange} />
 
         <Table.Body>
